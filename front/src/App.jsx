2f691be2df4941db8256156bd44279105d57698c
costar011 <dnaer456@gmail.com>
2022-02-04 22:21:33 +0900

    front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,3 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
+
+class App extends React.Component {
+  render() {
+    return <h1> hello </h1>;
+  }
+}
+
+export default App;
