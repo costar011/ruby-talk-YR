5995daa5de35ebb2776a10c6492835a878e4ad65
costar011 <dnaer456@gmail.com>
2022-02-05 09:44:37 +0900

    login

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
+import Login from "./Login";
+
+const ContentWrapper = styled.section`
+  width: 100%;
+  height: calc(100vh - 60px);
+
+  overflow: scroll;
+`;
 
 class App extends React.Component {
   render() {
-    return <h1> hello </h1>;
+    return (
+      <Routes>
+        <Route exaxt path="/" element={<Login />} />
+      </Routes>
+    );
   }
 }
 
---
+++ front/src/Login.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Button, message } from "antd";
+import { useNavigate } from "react-router-dom";
+
+class Login extends React.Component {
+  render() {
+    return <h1> login </h1>;
+  }
+}
+
+export default Login;
