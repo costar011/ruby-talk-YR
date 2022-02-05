ce0d82ef8dfd465876b241febbd9d050619aaa7c
costar011 <dnaer456@gmail.com>
2022-02-05 09:58:21 +0900

    login

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -2,6 +2,22 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Button, message } from "antd";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Whole = styled.section`
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AppTitle = styled.h2`
+  margin-bottom: 100px;
+`;
 
 class Login extends React.Component {
   render() {
