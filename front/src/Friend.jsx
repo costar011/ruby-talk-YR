60222ec98f7f9044c40fd73b3510d5f4a3486f6b
costar011 <dnaer456@gmail.com>
2022-02-07 21:12:32 +0900

    front friend

---
+++ front/src/Friend.jsx
@@ -0,0 +1,6 @@
+import React, { useEffect } from "react";
+import TopNav from "./TopNav";
+import { loginCheck } from "./middlewares";
+import { useNavigate } from "react-router-dom";
+
+export default Friend;
--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -19,14 +19,56 @@ const AppTitle = styled.h2`
   margin-bottom: 100px;
 `;
 
-class Login extends React.Component {
-  render() {
-    return (
-      <Whole>
-        <AppTitle>RUBY TALK</AppTitle>
-      </Whole>
-    );
+const InputGuide = styled.span`
+  font-size: 13px;
+  color: #999;
+`;
+
+const SignButton = styled(Button)`
+  width: 80%;
+  margin-bottom: 10px;
+`;
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [code, setCode] = useState("");
+  const [step, setStep] = useState(1);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const flag = localStorage.getItem("ruby_login");
+
+    if (flag === "true") {
+      navigate("/friend");
+    } else {
+      return;
+    }
+  }, []);
+
+  const onChangeInput = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangeCode = (event) => {
+    setCode(event.target.value);
+  };
+
+  const codeCheckAction = async () => {
+    await localStorage.setItem("ruby_login", true);
+    message.success("Welcom to RUBY TALK");
+    navigate("/friend");
+  };
+
+  class Login extends React.Component {
+    render() {
+      return (
+        <Whole>
+          <AppTitle>RUBY TALK</AppTitle>
+        </Whole>
+      );
+    }
   }
-}
+};
 
 export default Login;
