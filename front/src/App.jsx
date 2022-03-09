01eccd74f4a266a773075d47fcbc47065c9684f6
costar011 <dnaer456@gmail.com>
2022-03-09 20:06:08 +0900

    reset

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,18 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import Login from "./Login";
 
-const ContentWrapper = styled.section`
-  width: 100%;
-  height: calc(100vh - 60px);
-
-  overflow: scroll;
-`;
-
-const NavWrapper = styled.section`
-  width: 100%;
-  height: 60px;
-`;
-
 class App extends React.Component {
   render() {
     return (
